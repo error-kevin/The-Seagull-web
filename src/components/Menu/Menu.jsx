6e0022ff8757b52d511ffbb9ffef06a92743151d
Karan Agrawal <karan25.pvt@gmail.com>
2025-03-04 21:01:16 +0530

    updates

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -8,25 +8,9 @@ export const Menu = ({ className }) => {
       <Link to="/home">
         <p className="menu-home-text menu-txt">Home</p>
       </Link>
-      <Link to="/about">
-        <p className="menu-about-text menu-txt">Men</p>
+      <Link to="/products">
+        <p className="menu-about-text menu-txt">Products</p>
       </Link>
-      <Link to="/events">
-        <p className="menu-events-text menu-txt">Women</p>
-      </Link>
-
-      {/* <Link to="/members">
-        <p className="menu-members-text menu-txt">Our Team</p>
-      </Link> */}
-      {/* <Link to="/gallery">
-        <p className="menu-members-text menu-txt">Gallery</p>
-      </Link> */}
-      {/* <Link to="/sponsors">
-        <p className="menu-events-text menu-txt">Our Partners</p>
-      </Link> */}
-      {/* <Link to="/HonouraryGuidance"> */}
-      {/* <p className="menu-events-text menu-txt">Our Leadership</p> */}
-      {/* </Link> */}
     </div>
   );
 };
